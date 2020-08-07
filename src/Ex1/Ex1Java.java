aa922dfa5c52bc0a0117445d48de663f38bb8ba8
Caio Cesar <caio.bispo1999@gmail.com>
2020-08-07 10:08:13 -0300

    https://github.com/users/CaioCSB/projects/1

---
+++ src/Ex1/Ex1Java.java
@@ -0,0 +1,19 @@
+package Ex1;
+
+import java.util.Scanner;
+
+public class Ex1Java {
+	public static void main (String args[])
+	{
+		Scanner ler = new Scanner(System.in);
+		int dia,mes,ano,anof;
+		System.out.println("Dia em que você nasceu? ");
+		dia = ler.nextInt();
+		System.out.println("Mês em que você nasceu? ");
+		mes = ler.nextInt();
+		System.out.println("Ano em que você nasceu? ");
+		ano = ler.nextInt();
+		anof = dia + mes*30 + ano*365;
+		System.out.println(anof);
+	}
+}
